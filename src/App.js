59ec012127999301b147097666d964a45132e95f
Vilay Bende <71513950+vilay1702@users.noreply.github.com>
2021-09-11 22:46:49 +0530

    updated Header

--- src/App.js
+++ src/App.js
@@ -5,23 +5,23 @@ import {AboutUs, Apply, Home, Incubation, Login, Mentors, News, Policy, Programs
 function App() {
   return (
     <>
-      <Header />
       <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/aboutus" component={AboutUs} />
-          <Route path="/apply" component={Apply} />
-          <Route path="/login" component={Login} />
-          <Route path="/incubation" component={Incubation} />
-          <Route path="/mentors" component={Mentors} />
-          <Route path="/news" component={News} />
-          <Route path="/policy" component={Policy} />
-          <Route path="/programs" component={Programs} />
-          <Route path="/programs" component={Programs} />
-          <Route path="*" component={Login} />
-        </Switch>
+        <Header />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/aboutus" component={AboutUs} />
+            <Route path="/apply" component={Apply} />
+            <Route path="/login" component={Login} />
+            <Route path="/incubation" component={Incubation} />
+            <Route path="/mentors" component={Mentors} />
+            <Route path="/news" component={News} />
+            <Route path="/policy" component={Policy} />
+            <Route path="/programs" component={Programs} />
+            <Route path="/programs" component={Programs} />
+            <Route path="*" component={Login} />
+          </Switch>
+        <Footer />
       </Router>
-      <Footer />
     </>  
   );
 }
