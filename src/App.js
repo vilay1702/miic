4947b1e498b13808df609b472e224d433fe61a5a
Vilay <vilay17bende@gmail.com>
2021-10-01 00:50:54 +0530

    changed BrowserRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Header, Footer } from "./components";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import {
   AboutUs,
   Apply,
