73798a83f1864d6a1f3092a45d1b7f20b5aa9410
Vilay <vilay17bende@gmail.com>
2021-12-02 11:54:36 +0530

    updated toTop button

--- src/components/ToTop.js
+++ src/components/ToTop.js
@@ -5,7 +5,7 @@ const ToTop = () => {
   return (
     <abbr title="Back to Top">
       <a href="#back-to-top" id="to-top-button">
-        <img src="https://img.icons8.com/material-sharp/24/000000/circled-up.png" />
+        <img src="https://img.icons8.com/ios-filled/50/ffffff/circled-up.png" />
       </a>
     </abbr>
   );
