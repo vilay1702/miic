5240219b66890c34eeea9492b1bc1700903a7da9
Vilay <vilay17bende@gmail.com>
2022-01-29 14:02:48 +0530

    updated to top button

--- src/components/ToTop.js
+++ src/components/ToTop.js
@@ -5,7 +5,20 @@ const ToTop = () => {
   return (
     <abbr title="Back to Top">
       <a href="#back-to-top" id="to-top-button">
-        <img src="https://img.icons8.com/ios-filled/50/ffffff/circled-up.png" />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M5 11l7-7 7 7M5 19l7-7 7 7"
+          />
+        </svg>
       </a>
     </abbr>
   );
